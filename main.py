db0dda881c2ed1e82c7f27712983b09dc3b4973c
mityuk17 <mityukov.17@gmail.com>
2023-01-08 00:05:20 +0300

    Основоной функционал доделан

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import asyncio
-
-from pyrogram import Client
+from pyrogram import Client, errors
 import os
 api_id = 9411854
 api_hash = '499c76606cefdeadd4b1ece84a5a9932'
@@ -13,9 +12,10 @@ def get_messages():
     with open('messages_ids.txt','r',encoding='utf8') as file:
         messages = file.readlines()
     for i in range(len(messages)):
-        post_number = messages[i].split('/')[-1]
-        channel_link = messages[i].split('/')[1]
-        messages[i] =  tuple([channel_link,int(post_number)])
+        msg = messages[i].replace('https://', '')
+        post_number = msg.split('/')[-1]
+        channel_link = msg.split('/')[1]
+        messages[i] = tuple([channel_link,int(post_number)])
     return messages
 
 def get_Xchannels():
@@ -25,32 +25,47 @@ def get_Xchannels():
     with open('Xchannels_ids.txt', 'r', encoding='utf8') as file:
         channel_ids = file.readlines()
     for i in range(len(channel_ids)):
+        channel_ids[i] = channel_ids[i].replace('https://', '')
         channel_ids[i] = [channel_ids[i].rstrip().split('/')[-1], 0]
     return channel_ids
 
-
-async def get_message(message_info):
-    channel_messages = app.get_chat_history(message_info[0])
-    total_msg = await app.get_chat_history_count(message_info[0])
-    print(total_msg)
-    target = total_msg - message_info[1] + 1
-    i = 0
-    async for message_i in channel_messages:
-        print(message_i)
-        i += 1
-        if i == target:
-            message = message_i
-            return message
 async def main():
     await app.start()
     messages_for_forwarding = get_messages()
     channels_to_forward = get_Xchannels()
-    if not(messages_for_forwarding) or not(channels_to_forward):
-        print('Ошибка')
+    if not messages_for_forwarding:
+        print('Ошибка, не получены сообщения для пересылки')
         return False
-    for message_i in messages_for_forwarding:
-        message = await get_message(message_i)
-        await app.forward_messages(channels_to_forward[-1][0],from_chat_id=message_i[0], message_ids=message.id)
+    elif not channels_to_forward:
+        print('Ошибка, не получены каналы для пересылки')
+        return False
+    #Рассылка по каналам по порядку
+    while channels_to_forward and messages_for_forwarding:
+        message_i = messages_for_forwarding.pop()
+        print(message_i[0], message_i[1])
+        try:
+            await app.forward_messages(channels_to_forward[-1][0],from_chat_id=message_i[0], message_ids=message_i[1])
+            channels_to_forward[ -1 ][ 1 ] += 1
+        except errors.exceptions.bad_request_400.MessageIdInvalid:
+            print(f'Не найдено сообщение по ссылке t.me/{message_i[0]}/{message_i[1]}')
         if channels_to_forward[-1][1] ==17:
             channels_to_forward.pop()
+
+    #Рассылка по каналам равномерно
+    # while channels_to_forward:
+    #     for index,channel in enumerate(channels_to_forward):
+    #         if channel[1] > 17:
+    #             channels_to_forward.remove(channel)
+    #             continue
+    #         msg = messages_for_forwarding.pop()
+    #         try:
+    #             await app.forward_messages(channel[0], from_chat_id=msg[0], message_ids=msg[1])
+    #             channels_to_forward[index][1] += 1
+    #         except errors.exceptions.bad_request_400.MessageIdInvalid:
+    #             print(f'Не найдено сообщение по ссылке t.me/{msg[0]}/{msg[1]}')
+
+    if messages_for_forwarding:
+        print(f'Закончились каналы для пересылки, осталось не пересланных сообщений: {len(messages_for_forwarding)}')
+    else:
+        print('Все сообщения были пересланы')
 asyncio.run(main())
