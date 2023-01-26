63ebaaef111e9536333390d7ecdadfa80ce9dab0
mityuk17 <87587511+mityuk17@users.noreply.github.com>
2023-01-26 20:57:37 +0300

    Update create_session.py

--- create_session.py
+++ create_session.py
@@ -1,7 +1,7 @@
 import telethon
 import os
-api_id = 9411854
-api_hash = '499c76606cefdeadd4b1ece84a5a9932'
+api_id =0
+api_hash = ''
 while True:
     name = input('Введите название для новой сессии')
     if os.path.exists(f'sessions/{name}.session'):
